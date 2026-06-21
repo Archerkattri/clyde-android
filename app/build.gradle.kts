ab033a772e52a39f16215792e0beece2a69be709
Archerkattri <176878924+Archerkattri@users.noreply.github.com>
2026-06-22 02:17:14 +0530

    release: 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "dev.kris.clyde"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         vectorDrawables { useSupportLibrary = true }
     }
 
